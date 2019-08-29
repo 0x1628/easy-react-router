fba96deb64b5a8be412736206674a730d8714949
fragment0 <hi@fragment0.com>
2019-08-29 16:59:40 +0800

    downgrade lib to es3

--- src/history.ts
+++ src/history.ts
@@ -1,17 +1,17 @@
 import {EventEmitter} from './utils'
 
 class History {
-  _length = 0
+  length = 0
   _lasttm: number | null = null
   back() {
     this.go(-1)
   }
   go(len: number): void {
     window.history.go(len)
-    this._length = Math.max(0, this._length - len)
+    this.length = Math.max(0, this.length - len)
   }
   push(url: string): void {
-    this._length += 1
+    this.length += 1
     const tm = Date.now()
     this._lasttm = tm
     window.history.pushState({
@@ -23,14 +23,8 @@ class History {
     if (!e.state) return false
     return e.state.tm === this._lasttm
   }
-  get length() {
-    return this._length
-  }
-  set length(_) {
-    throw new Error('history.length is readonly')
-  }
   unsafe_setLength(len: number) {
-    this._length = len
+    this.length = len
   }
 }
 
