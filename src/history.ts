7e1ab8755d7114b31624087e35e20526da584203
fragment0 <hi@fragment0.com>
2019-10-31 21:40:48 +0800

    add replaceState & listener

--- src/history.ts
+++ src/history.ts
@@ -1,8 +1,11 @@
 import {EventEmitter} from './utils'
 
+type ChangeListner = () => void
+
 class History {
   length = 0
   _lasttm: number | null = null
+  _changeListeners: ChangeListner[] = []
   back() {
     this.go(-1)
   }
@@ -18,6 +21,16 @@ class History {
       tm,
     }, '', url)
     EventEmitter.dispatchEvent('pushstate')
+    this.unsafe_fireChange()
+  }
+  replace(url: string): void {
+    const tm = Date.now()
+    this._lasttm = tm
+    window.history.replaceState({
+      tm,
+    }, '', url)
+    EventEmitter.dispatchEvent('pushstate')
+    this.unsafe_fireChange()
   }
   isLastUrl(e: PopStateEvent) {
     if (!e.state) return false
@@ -26,6 +39,19 @@ class History {
   unsafe_setLength(len: number) {
     this.length = len
   }
+  unsafe_fireChange() {
+    this._changeListeners.forEach(item => item())
+  }
+  addChangeListener(callback: ChangeListner) {
+    this._changeListeners.push(callback)
+  }
 }
 
 export const history = new History()
+
+// tslint:disable-next-line
+if (typeof window !== 'undefined' && window.addEventListener) {
+  window.addEventListener('popstate', () => {
+    history.unsafe_fireChange()
+  })
+}
